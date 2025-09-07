421b6b09857ec1ba773320dab8f792628f7172b6
John Carlo <carlo9carbonels@mail.com>
2025-09-08 03:37:36 +0800

    Added preloader back

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -80,9 +80,9 @@ const PageWrapper = ({ children }: { children: React.ReactNode }) => {
           </div>
         </>
       )}
-      {/* {preloaders.map((p) => (
+      {preloaders.map((p) => (
         <PreLoader key={p} pathname={p} preloaders={preloaders} />
-      ))} */}
+      ))}
     </>
   );
 };
